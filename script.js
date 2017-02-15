3860775539e02e6081be91a7f87b235dcb4c19bb
nicfue <nicolasfuentes@hotmail.com>
2017-02-15 21:55:40 +0100

    uppdaterat

--- script.js
+++ script.js
@@ -1,37 +1,46 @@
+//skapa två listor
+ var myList = document.getElementById('myList');
+ var myList2 = document.getElementById('myList2');
 //skapa ett inmatningsfält för att lägga till sysslor
 function addTodo() {
 var stuff = document.getElementById('todoInput').value;
  var text = document.createTextNode(stuff);
  var newStuff = document.createElement('li');
- newStuff.appendChild(text); 
-document.getElementById('myList').appendChild(newStuff);
+ newStuff.className = "listItems";
+ newStuff.appendChild(text);
+ myList.appendChild(newStuff);
 
 //skapa en "ta bort knapp" på listan
-var update = document.createElement("update");
+var update = document.createElement("div");
   var removeButton = document.createTextNode("\u00D7");
   update.className = "remove";  
   update.appendChild(removeButton);
   newStuff.appendChild(update);
-
-
-//tar bort en syssla när man trycker på knappen
-removeButton = document.getElementsByClassName("remove");
-var i;
-for (i = 0; i < removeButton.length; i++) {
-    removeButton[i].onclick = function() {
-    var div = this.parentElement;
-    };
 }
 
-  for (i = 0; i < removeButton.length; i++) {
-    removeButton[i].onclick = function() {
+myList.addEventListener("mouseover", removeToDo);
+ //tar bort en syssla från första listan
+function removeToDo() {
+var remove = document.getElementsByClassName("remove");
+var i;
+  for (i = 0; i < remove.length; i++) {
+    remove[i].onclick = function() {
       var div = this.parentElement;
-      div.style.display = "none";
+      myList2.appendChild(div);
     };
   }
-
-  stuff.insertBefore(newStuff, childhood[0]);
 }
 
+myList2.addEventListener("mouseover", doneList);
+function doneList() {
+var remove = document.getElementsByClassName("remove");
+var i;
+  for (i = 0; i < remove.length; i++) {
+    remove[i].onclick = function() {
+      var div = this.parentElement;
+      myList2.removeChild(div); 
+    };
+  }
+}  
 
 
